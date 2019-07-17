672963db5faa336221a9ed1f43590f3d621536c0
mites1997 <51950018+mites1997@users.noreply.github.com>
2019-07-17 16:31:02 +0530

    Update Actor.java

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -21,4 +21,11 @@ public class Actor
     public void setAge(int age) {
         this.age = age;
     }
+    @Override
+   public String toString() {
+       return "Actor{" +
+               "name='" + name + '\'' +
+               ", gender='" + gender + '\'' +
+               ", age=" + age +
+               '}';
 }
